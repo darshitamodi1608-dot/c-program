fc1b90efc1d586cd5bbf5ee89bc9ad1984bd5ba3
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-19 11:32:07 +0530

    multiply of two numbers

---
+++ simple_programs/multiple.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int n1,n2,multi;
+	printf("Enter two number");
+	scanf("%d %d",&n1,&n2);
+	multi=n1*n2;
+	printf("Multi of two number is %d",multi);
+	return 0;
+}
