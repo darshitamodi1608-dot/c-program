b5b0111d5dd5f4a689f4f3e17fbc178673d7beb9
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-20 12:44:11 +0530

    pattern 1

--- loops_pattern/pattern_1.cpp
+++ loops_pattern/pattern_1.cpp
@@ -1,16 +1,15 @@
 #include<stdio.h>
 int main()
 {
-	int i,j;
-	for(i='A';i<='H';i++)
-	{
-		for(j=64;j<='F';j++)
-		  if(i=='A'|| i=='H'|| j<i-1)
-		   printf("*");
-		  else
-		   printf("%c",j);
-		   printf("\n"); 
-	}
-	
-	return 0;
+ int i,j;
+ for (i='A';i<='H';i++)
+ {
+  for (j=64;j<='F';j++)
+  if(i=='A'i=='H'j<i-1)
+  printf("*");
+  else
+  printf("%c",j);
+  printf("\n");
+ }
+ return 0;
 }
