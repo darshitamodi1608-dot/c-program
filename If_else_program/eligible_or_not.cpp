13083edf642355d46ea0fb79d02280dd270f72fd
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-23 13:56:07 +0530

    added to if_else_program

---
+++ If_else_program/eligible_or_not.cpp
@@ -0,0 +1,13 @@
+/* check voting Eligibility */
+#include<stdio.h>
+int main()
+{
+	int age;
+	printf("Enter age:");
+	scanf("%d",&age);
+	if(age>=18)	
+	 printf("Eligible for vote");
+	else 
+     printf("Not Eligible");
+	return 0; 
+}
