bd73b0c0c9adc057c9b0698556bde1d8313865b0
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-23 13:52:10 +0530

    added to if_else_program

---
+++ If_else_program/leap_year.cpp
@@ -0,0 +1,13 @@
+/* check it is leap year or not */
+#include<stdio.h>
+int main()
+{
+	int year;
+	printf("Enter year:");
+	scanf("%d",&year);
+	if(year%400==0||year%100==1||year%4==0)	
+	 printf("Leap year");
+	else 
+     printf("Not leap year");
+	return 0; 
+}
