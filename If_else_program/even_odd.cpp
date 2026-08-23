7e529a64c0ce81b98a232389963faf2ac9283792
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-23 13:02:58 +0530

    added to if_else_program

---
+++ If_else_program/even_odd.cpp
@@ -0,0 +1,13 @@
+/* check number is even or odd*/
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter number");
+	scanf("%d",&n);
+	if(n%2==0)
+	 printf("Number is even");
+	else
+	 printf("Number is odd"); 
+	return 0; 
+}
