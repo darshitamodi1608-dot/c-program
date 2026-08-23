1954cc47eb90b5522525dcd1efad3713193ddc1b
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-23 13:53:00 +0530

    added to if_else_program

---
+++ If_else_program/largest_2.cpp
@@ -0,0 +1,16 @@
+/* check which number is largest in 2 number */
+#include<stdio.h>
+int main()
+{
+	int n1,n2;
+	printf("Enter number 1:");
+	scanf("%d",&n1);
+	printf("Enter number 2 :");
+	scanf("%d",&n1);
+	if(n1<n2)
+	 printf("N1 is largest");
+	else 
+	 printf("N2 is largest");
+	 
+	return 0; 
+}
