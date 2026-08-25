b36ba34b8233d50d9a43db78c37f0848f836efb2
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-25 10:48:03 +0530

    added to operators

---
+++ operator/Relational_operators.cpp
@@ -0,0 +1,17 @@
+/* Relational operators */
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter number1 : ");
+	scanf("%d",&a);
+	printf("Enter number2 : ");
+	scanf("%d",&b);
+	printf("\n a>b %d ",a>b);
+	printf("\n a<b %d ",a<b);
+	printf("\n a>=b %d ",a>=b);
+	printf("\n a<=b %d ",a<=b);
+	printf("\n a==b %d ",a==b);
+	printf("\n a!=b %d ",a!=b);
+	return 0;	
+}
