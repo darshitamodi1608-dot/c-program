6fc17e14dd715066362b09803dbbb1afff1ae0ae
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-26 12:35:54 +0530

    added to switch case

---
+++ Switch_case/month_name.cpp
@@ -0,0 +1,63 @@
+/* Month name*/
+#include <stdio.h>
+int main()
+{ 
+    int day;
+    printf("Enter month number(1 to 12): ");
+    scanf(" %d", &day);
+    switch(day)
+    {
+        case 1:
+        	printf("January");
+            break;
+               
+		case 2:
+		    printf("february");
+            break;
+               
+		case 3:
+		    printf("March");
+            break;
+               
+		case 4:
+		    printf("April");
+            break;
+               
+		case 5:
+		    printf("May");
+            break;
+               
+		case 6:
+		    printf("June");
+            break;
+               
+		case 7:
+		    printf("July");
+            break;
+            
+        case 8:
+		    printf("August");
+            break;
+			
+		case 9:
+		    printf("Septembar");
+            break;
+			
+		case 10:
+		    printf("October");
+            break;
+			
+		case 11:
+		   printf("November");
+           break;
+			
+	    case 12:
+		    printf("December");
+            break;    
+               
+        default:
+            printf("Invalid month number");
+    }
+
+    return 0;
+}
