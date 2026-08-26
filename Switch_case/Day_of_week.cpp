f6d6872a805b7ff08af9f368131e5c5e0dc9928c
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-26 12:37:27 +0530

    added to switch case

---
+++ Switch_case/Day_of_week.cpp
@@ -0,0 +1,43 @@
+/* Day of week */
+#include <stdio.h>
+int main()
+{ 
+    int day;
+    printf("Enter day of week: ");
+    scanf(" %d", &day);
+    switch(day)
+    {
+        case 1:
+        	printf("Monday");
+            break;
+               
+		case 2:
+		    printf("Tuesday");
+            break;
+               
+		case 3:
+		    printf("Wednesday");
+            break;
+               
+		case 4:
+		    printf("Thrusday");
+            break;
+               
+		case 5:
+		    printf("Friday");
+            break;
+               
+		case 6:
+		    printf("Saturday");
+            break;
+               
+		case 7:
+		    printf("Sunday");
+            break;
+               
+        default:
+            printf("Invalid day");
+    }
+
+    return 0;
+}
