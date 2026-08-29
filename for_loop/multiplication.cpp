c7aea0efeaf1ef009ce4a3fd9aea061ac7a67fe8
darshitamodi1608-dot <darshitamodi1608@gmail.com>
2026-08-29 21:17:55 +0530

    added to for loop

---
+++ for_loop/multiplication.cpp
@@ -0,0 +1,12 @@
+/*Multiplication of tables*/
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	printf("Enter number");
+	scanf("%d",&n);
+	for(i=1;i<=10;i++)
+	printf("\n%d*%d=%d",n,i,n*i); 
+	printf("\n");
+	return 0;
+}
